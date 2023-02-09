689a71526c775fa7d46b0165c2e7fde44a6e95ae
xnayan00 <yan_mendes.18@outlook.com>
2023-02-09 17:50:14 -0300

    atualizando o mutation types

--- frontend/src/store/mutation-types.js
+++ frontend/src/store/mutation-types.js
@@ -33,6 +33,7 @@ export const MUTATE_TEACHERS = 'MUTATE_TEACHERS'
 //==============================
 export const GET_USER = 'GET_USER'
 export const GET_USERS = 'GET_USERS'
+export const UPDATE_USER = 'UPDATE_USER'
 export const SET_USER = 'SET_USER'
 export const SET_USERS = 'SET_USERS'
 export const MUTATE_USER = 'MUTATE_USER'
