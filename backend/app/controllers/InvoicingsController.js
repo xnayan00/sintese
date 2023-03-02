0a13b68497635261630a894d58b3a28c46ef480b
xnayan00 <yan_mendes.18@outlook.com>
2023-03-02 15:36:00 -0300

    Criando a controller de entradas

---
+++ backend/app/controllers/InvoicingsController.js
@@ -0,0 +1,40 @@
+const InvoicingsModel = require("../models/InvoicingsModel")
+
+module.exports.index = async (req, res) => {
+    try {
+        const invoicings = await InvoicingsModel.find().sort({ createdAt: -1 })
+
+        if(invoicings.length == 0) return res.status(200).json({msg: "Nenhum registro encontrado", data: []})
+    
+        await res.status(201).json({msg: "registros recuperados com sucesso.", data: invoicings})
+    } catch (err) {
+        await res.status(500).json(err.message)
+    } 
+}
+
+module.exports.store = async (req, res) => {
+    const data = req.body
+
+    try {
+        const storedInvoicing = await InvoicingsModel.create(data)
+    
+        await res.status(201).json({msg: "Registro cadastrado com sucesso.", data: storedInvoicing})
+    } catch (err) {
+        await res.status(500).json(err.message)
+    }
+
+}
+
+module.exports.destroy = async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const deletedInvoicing = await InvoicingsModel.findByIdAndDelete(id)
+    
+        if(!deletedInvoicing) return res.status(404).json({msg: "Registro não encontrado"})
+    
+        await res.status(200).json({msg: "Registro excluído com sucesso.", data: deletedInvoicing})
+    } catch (err) {
+        await res.status(500).json(err.message)
+    }    
+}
